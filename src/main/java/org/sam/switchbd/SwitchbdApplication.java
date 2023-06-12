8c6dade5ff65d269052969dd940e1a112b978861
Christian <chriscar.sam@gmail.com>
2023-06-12 10:06:59 -0500

    New Project

---
+++ src/main/java/org/sam/switchbd/SwitchbdApplication.java
@@ -0,0 +1,13 @@
+package org.sam.switchbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sam/switchbd/SwitchbdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sam.switchbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
