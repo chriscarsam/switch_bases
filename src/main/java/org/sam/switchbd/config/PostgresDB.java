e4dcd35603f6d29d15877f4978f03b7753340e91
Christian <chriscar.sam@gmail.com>
2023-06-12 11:33:16 -0500

    PostgresDB configuración

--- src/main/java/org/sam/switchbd/config/PostgresDB.java
+++ src/main/java/org/sam/switchbd/config/PostgresDB.java
@@ -1,8 +1,57 @@
 package org.sam.switchbd.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(entityManagerFactoryRef = "postgresEntityManagerFactory",
+transactionManagerRef = "postgresTransactionManager", basePackages = {
+        "org.sam.switchbd.repository.postgres"
+})
 public class PostgresDB {
+
+    @Value("${postgres.jpa.database-platform}")
+    private String dialect;
+
+    @Bean(name = "postgresDatasource")
+    @ConfigurationProperties(prefix = "postgres.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "postgresEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(
+            @Qualifier("postgresDatasource") DataSource dataSource){
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setPackagesToScan("org.sam.switchbd.entity.postgres");
+        HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+        factoryBean.setJpaVendorAdapter(vendor);
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.dialect", dialect);
+        factoryBean.setJpaPropertyMap(properties);
+        return factoryBean;
+    }
+
+    @Bean("postgresTransactionManager")
+    public PlatformTransactionManager platformTransactionManager(
+            @Qualifier("postgresEntityManagerFactory") EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 }
